59ecd870935d3677aa2c0f0edc052395ed1c6b0d
huguangyuan <3625136863@qq.com>
2025-06-06 10:30:11 +0800

    feat:层级模型学习完了

--- ThreeJS/04_层级模型/src/mesh.js
+++ ThreeJS/04_层级模型/src/mesh.js
@@ -1,16 +1,12 @@
 import * as THREE from 'three'
 
-const geometry=new THREE.BoxGeometry(20,20,20)
+const geometry=new THREE.BoxGeometry(50,50,50)
 
 
 const material=new THREE.MeshLambertMaterial({
   color:0x00ffff,
 })
-
 const mesh=new THREE.Mesh(geometry,material)
-
-mesh.position.x=50
-const group=new THREE.Group()
-group.add(mesh)
-group.position.x=50
-export default group 
+geometry.translate(50/2,0,0)
+ mesh.rotateY(Math.PI/4)
+export default mesh 
